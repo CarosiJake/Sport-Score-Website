f68a33a30323fec97d1c5deceb1031295ed1de6d
Jake Carosi Primes <carosiprimesjake@yahoo.com>
2023-05-11 16:02:12 -0400

    Modified index.html, mlbScores.html; renamed script.js to mlbScript.js; new file nbaScores.html, nbaScripts.js, nhlScores.html, nhlScript.js

---
+++ nbaScripts.js
@@ -0,0 +1,37 @@
+$.getJSON(
+    "", 
+        function mlbData(data) {
+        console.log(data);
+        
+
+
+
+        var todayDate = data.dates[0].date;
+        $('.todayDate').append(todayDate);
+
+
+        for (var i = 0; i < data.dates[0].games.length; i++){
+            var games = data.dates[0].games[i];
+            //console.log(games) 
+            var awayTeam = games.teams.away.team.name;
+            var awayScore = games.teams.away.score;
+            var homeTeam = games.teams.home.team.name;
+            var homeScore = games.teams.home.score;
+            //console.log(awayTeam + ' ' + awayScore + ' vs ' + homeTeam + ' ' + homeScore)
+            var vs = awayTeam + ' ' + awayScore + ' at ' + homeTeam + ' ' + homeScore + '<br>';
+            
+            $('.games').append(vs);
+            }
+        
+
+
+
+
+
+
+
+        
+
+
+    }
+);
